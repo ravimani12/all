b0c253777545fa52c3eb21cbb0339e5190223f3c
ravimani12 <manikandan30796@gmail.com>
2021-12-11 17:45:06 +0530

    done

---
+++ src/org/all/AllWelcome.java
@@ -0,0 +1,11 @@
+package org.all;
+
+public class AllWelcome {
+	private void add() {
+System.out.println("add");
+	}
+public static void main(String[] args) {
+	AllWelcome a = new AllWelcome();
+	a.add();
+}
+}
